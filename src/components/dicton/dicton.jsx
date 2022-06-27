56afb170f602cfda7a1ee579c047dd6fc13c2c60
delvauxo <delvauxo@outlook.com>
2022-06-27 19:24:25 +0200

    generic margin-top + margin-top applied on dicton component

--- src/components/dicton/dicton.jsx
+++ src/components/dicton/dicton.jsx
@@ -3,13 +3,13 @@ import { Container } from '@mui/material';
 const Dicton = ({ container, dicton }) => {
 
     return container ? (
-        <Container>
+        <Container className='mt-lg'>
             <div className='rounded-bg-white'>
                 {dicton && <p className='font-brittany secondary-color txt-lg'>{dicton}</p>}
             </div>
         </Container>
     ) : (
-        <div className='rounded-bg-white'>
+        <div className='rounded-bg-white mt-lg'>
             {dicton && <p className='font-brittany secondary-color txt-lg'>{dicton}</p>}
         </div>
     );
