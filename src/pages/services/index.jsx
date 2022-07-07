e889ad1669c07fa530f6c2f0ca1197030cfd765f
delvauxo <delvauxo@outlook.com>
2022-07-07 04:08:10 +0200

    services page styled

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,5 +1,8 @@
+import { Container } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Dicton from '../../components/dicton/dicton';
+import PastilleSlider from '../../components/slider/slider';
 
 const ServicesPage = () => {
 
@@ -14,18 +17,31 @@ const ServicesPage = () => {
 
     const servicesJSX = services.map(
         service => (
-            <li key={service.id}>
-                <div>{service.nom}</div>
-                <div>{service.description}</div>
+            <li key={service.id} className='rounded-bg-white'>
+                <div className='desc-text font-chakra'>
+                    <div className='txt-xxl tertiary-color'>{service.nom}</div>
+                    <div className='secondary-color mt-sm'>{service.description}</div>
+                </div>
+                <div className='desc-image'>
+                    <img src={`${process.env.REACT_APP_BACK_URL}/services/images/` + service.image} alt={service.nom} />
+                </div>
             </li>
         )
     );
 
     return (
         <>
-            <ul>
-                {servicesJSX}
-            </ul>
+            <div className="slider-component border-effect-white-bt no-margin">
+                <img className="bg-image" src={`${process.env.REACT_APP_BACK_URL}/components_bg/flower.jpg`} alt="background" />
+                <PastilleSlider items={services} name="services" body={false} />
+            </div>
+            <Dicton container={true} dicton='« Dicton Services !!! »' />
+
+            <Container>
+                <ul className='component-list no-margin'>
+                    {servicesJSX}
+                </ul>
+            </Container>
         </>
     );
 };
