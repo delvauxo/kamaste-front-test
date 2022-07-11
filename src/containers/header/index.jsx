d773d60e77a5471b762552459810a1d0a4a3c4ed
delvauxo <delvauxo@outlook.com>
2022-07-11 03:27:38 +0200

    added container to header nav

--- src/containers/header/index.jsx
+++ src/containers/header/index.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, Toolbar } from '@mui/material';
+import { AppBar, Button, Container, Toolbar } from '@mui/material';
 import { Box } from '@mui/system';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -16,36 +16,42 @@ const Header = () => {
         <header>
             <AppBar position='static' sx={{ bgcolor: "#657ead" }}>
                 <Toolbar sx={{ minHeight: 36, height: 100 }}>
-                    <Link to={'/'}>
-                        <img src="/images/logo_k.png" alt="Kamaste logo" width={75} />
-                    </Link>
-                    <Box sx={{ display: 'flex', flexGrow: 1, alignItems: 'center', gap: '20px', ml: 3 }}>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body' end>
-                            <span>Body</span>
-                        </NavLink>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/about'>
-                            <span>Qui sommes-nous ?</span>
-                        </NavLink>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/moments'>
-                            <span>Moments</span>
-                        </NavLink>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/equipements'>
-                            <span>Équipements</span>
-                        </NavLink>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/services'>
-                            <span>Services</span>
-                        </NavLink>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/reserver'>
-                            <span>Réserver</span>
-                        </NavLink>
-                        <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/contact'>
-                            <span>Contact</span>
-                        </NavLink>
+                    <Box sx={{ position: 'absolute' }}>
+                        <Link to={'/'}>
+                            <img src="/images/logo_k.png" alt="Kamaste logo" width={75} />
+                        </Link>
                     </Box>
+                    <Container>
+                        <Box sx={{ display: 'flex', flexGrow: 1, alignItems: 'center', gap: '20px', ml: 3 }}>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body' end>
+                                <span>Body</span>
+                            </NavLink>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/about'>
+                                <span>Qui sommes-nous ?</span>
+                            </NavLink>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/moments'>
+                                <span>Moments</span>
+                            </NavLink>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/equipements'>
+                                <span>Équipements</span>
+                            </NavLink>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/services'>
+                                <span>Services</span>
+                            </NavLink>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/reserver'>
+                                <span>Réserver</span>
+                            </NavLink>
+                            <NavLink className={classNames(style.fontBtny, style.navLink)} to='/body/contact'>
+                                <span>Contact</span>
+                            </NavLink>
+                        </Box>
+                    </Container>
                     {
                         !user.token ? (<>
-                            <Button color='inherit' component={NavLink} to='/register'>Register</Button>
-                            <Button color='inherit' component={NavLink} to='/login'>Login</Button>
+                            <Box className='d-none'>
+                                <Button color='inherit' component={NavLink} to='/register'>Register</Button>
+                                <Button color='inherit' component={NavLink} to='/login'>Login</Button>
+                            </Box>
                         </>) : (
                             <>
                                 <Button color='inherit' component='div'>
